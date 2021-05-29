b637ecfce69cf3f8420f81ea7b2c236ca553cd3a
lionxu <xuzuhai163@163.com>
2021-05-29 23:22:23 +0800

    greedy 2021-05-29

---
+++ leetcode/canJump.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums){
+        int n = nums.size();
+
+        int farthest = 0;
+
+        for (int i = 0; i < n - 1; ++i) {
+            farthest = max(farthest, i + nums[i]);
+
+            if (farthest <= i) return false;
+        }
+
+        return farthest >= n - 1;
+    }
+};
