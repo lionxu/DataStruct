6df91a8edb5ea526b1ba681c89c4f1438fcaf412
lionxu <xuzuhai163@163.com>
2021-06-17 22:50:57 +0800

    2021-06-17

---
+++ leetcode/canPlaceFlowers.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int> &flowerbed, int n) {
+        flowerbed.insert(flowerbed.begin(), 0);
+        flowerbed.push_back(0);
+
+        int count = 0;
+
+        for (int i = 1; i < flowerbed.size() - 1; ++i) {
+            if (flowerbed[i] == 0 && flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0) {
+                flowerbed[i] = 1;
+                count++;
+            }
+        }
+
+        return count >= n;
+    }
+};
+
+int main() {
+    vector<int> flowerbed{1, 0, 0, 0, 1};
+
+    Solution solution;
+
+    cout << solution.canPlaceFlowers(flowerbed, 1) << endl;
+
+    return 0;
+}
