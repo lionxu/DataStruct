87e6f7c8f2224a60cd79e741bd26ee818d21ea9f
lionxu <xuzuhai163@163.com>
2021-07-02 23:36:43 +0800

    2021-07-02

--- leetcode/isMatch.cpp
+++ leetcode/isMatch.cpp
@@ -14,11 +14,19 @@ public:
 
         dp[0][0] = true;
 
-        for (int i = 0; i <= m; ++i) {
+        for (int i = 1; i <= n; ++i) {
+            if (p[i - 1] == '*') {
+                dp[0][i] = dp[0][i - 2];
+            }
+        }
+
+
+        for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
-                if (s[i] == p[j] || p[j] == '.') dp[i][j] = dp[i - 1][j - 1];
-                else if (p[j] == '*') {
-                    if (s[i] == p[j - 1] || p[j - 1] == '.') dp[i][j] = dp[i][j - 1] || dp[i - 1][j] || dp[i][j - 2];
+                if (s[i - 1] == p[j - 1] || p[j - 1] == '.') dp[i][j] = dp[i - 1][j - 1];
+                else if (p[j - 1] == '*') {
+                    if (s[i - 1] == p[j - 2] || p[j - 2] == '.')
+                        dp[i][j] = dp[i][j - 1] || dp[i - 1][j] || dp[i][j - 2];
                     else dp[i][j] = dp[i][j - 2];
                 } else {
                     dp[i][j] = false;
@@ -30,11 +38,11 @@ public:
     }
 };
 
-int main() {
+int test_isMatch() {
     Solution solution;
 
-    string s = "aa";
-    string p = "a";
+    string s = "aab";
+    string p = "c*a*b";
 
     cout << solution.isMatch(s, p) << endl;
 
