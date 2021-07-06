d17014b4d41d94787cd42a0f5b90ff7373c0d43f
lionxu <xuzuhai163@163.com>
2021-07-06 21:02:29 +0800

    2021-07-06

---
+++ leetcode/isMatch_2.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.length();
+        int n = p.length();
+
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+
+        dp[0][0] = true;
+
+        for (int i = 1; i <= n; ++i) {
+            if (p[i - 1] == '*') {
+                dp[0][i] = true;
+            } else {
+                break;
+            }
+        }
+
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (s[i - 1] == p[j - 1] || p[j - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+};
+
+int test_isMatch() {
+    Solution solution;
+
+    string s = "aa";
+    string p = "a";
+
+    cout << solution.isMatch(s, p) << endl;
+
+    return 0;
+}
