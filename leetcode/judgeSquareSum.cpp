2fd8902e2ed72e9820968c9554f6a7fc4f2b7df0
lionxu <xuzuhai163@163.com>
2021-08-28 14:58:17 +0800

    2021-08-28

---
+++ leetcode/judgeSquareSum.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    static bool judgeSquareSum(int c) {
+        long left = 0;
+        long right = (long ) sqrt(c);
+
+        while (left <= right) {
+            long sum = left * left + right * right;
+
+            if (sum == c) {
+                return true;
+            } else if (sum < c) {
+                left++;
+            } else if (sum > c) {
+                right++;
+            }
+        }
+
+        return false;
+    }
+};
+
+int main() {
+    int c = 5;
+
+    cout << Solution::judgeSquareSum(c) << endl;
+
+    return 0;
+}
