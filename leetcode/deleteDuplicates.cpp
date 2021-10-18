65ceb39eba59a835d1844851a5078fee4d16c990
lionxu <xuzuhai163@163.com>
2021-10-19 06:39:29 +0800

    2021-10-19

--- leetcode/deleteDuplicates.cpp
+++ leetcode/deleteDuplicates.cpp
@@ -2,26 +2,19 @@
 
 class Solution {
 public:
-    static ListNode *deleteDuplicates(ListNode *head) {
+    static ListNode* deleteDuplicates(ListNode *head) {
         if (!head) return head;
 
-        auto *dummy = new ListNode(-1, head);
+        ListNode *curr = head;
 
-        auto *curr = dummy;
-
-        while (curr->next && curr->next->next) {
-            if (curr->next->val == curr->next->next->val) {
-                int x = curr->next->val;
-
-                while (curr->next && curr->next->val == x) {
-                    curr->next = curr->next->next;
-                }
-            }
-            else {
+        while (curr->next) {
+            if (curr->val == curr->next->val) {
+                curr->next = curr->next->next;
+            } else {
                 curr = curr->next;
             }
         }
 
-        return dummy->next;
+        return head;
     }
 };
